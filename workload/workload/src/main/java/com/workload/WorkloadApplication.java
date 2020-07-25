4faf400f0967fe9fbc9a6d4d7acb90724d4a4dc3
bhargava <bhargava@DESKTOP-3BEJU8N>
2020-07-25 14:43:11 -0500

    WorkloadApplication

--- workload/workload/src/main/java/com/workload/WorkloadApplication.java
+++ workload/workload/src/main/java/com/workload/WorkloadApplication.java
@@ -3,12 +3,9 @@ package com.workload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.workload.repository.OrderRepository;
-
 @SpringBootApplication
 public class WorkloadApplication {
-	@Autowired
-	OrderRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkloadApplication.class, args);
 	}
