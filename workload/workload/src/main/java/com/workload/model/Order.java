1a79124e589065a9e6dfa1f0e2a2e19bcc08692b
bhargava <bhargava@DESKTOP-3BEJU8N>
2020-07-25 14:00:37 -0500

    Added changes

--- workload/workload/src/main/java/com/workload/model/Order.java
+++ workload/workload/src/main/java/com/workload/model/Order.java
@@ -1,5 +1,10 @@
 package com.workload.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Order")
 public class Order {
 
 
--- workload/workload/src/main/java/com/workload/repository/OrderRepository.java
+++ workload/workload/src/main/java/com/workload/repository/OrderRepository.java
@@ -1,11 +1,12 @@
 package com.workload.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.workload.model.Order;
 
 
-
+@Component("WorkloadService")
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
